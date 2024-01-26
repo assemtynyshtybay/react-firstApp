c7a9e9685212fff9ffaf1c9056cc51b5593022ad
assemtynyshtybay <47156211+assemtynyshtybay@users.noreply.github.com>
2024-01-26 12:09:25 +0600

    init: init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="profile">
+      <h1 style={{color: "lightcoral"}}>Assem Tynyshtybay</h1>
+      <p style={{color: "darkslateblue"}}>Frontend developer</p>
+      <p style={{color: "lightseagreen"}}>23 y.o., Almaty</p>
+    </div>
+  );
+}
+
+export default App;
